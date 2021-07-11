c7e858b5ac28e9b96c81606597512f7ec1162c90
shreyanshp25 <shreyansh2511@gmail.com>
2021-07-12 00:20:10 +0530

    added navbar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CustomerComponentComponent } from './components/customer-component/customer-component.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'customers', component: CustomerComponentComponent},
+  {path: '', redirectTo: 'customers', pathMatch: 'full'},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
