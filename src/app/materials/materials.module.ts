453e436c39736a5feab9397d413c9174c2968b30
shreyanshp25 <shreyansh2511@gmail.com>
2021-07-15 22:55:34 +0530

    data binding added for add customer

--- src/app/materials/materials.module.ts
+++ src/app/materials/materials.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import {MatTableModule} from '@angular/material/table';
+import { MatInputModule } from '@angular/material/input';
+
 
 const materialComponents = [
   MatTableModule, 
-  MatFormFieldModule
+  MatFormFieldModule,
+  FormsModule,
+  MatInputModule,
+  ReactiveFormsModule
 ];
 
 @NgModule({
